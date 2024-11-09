07818dcb3e1f545a6eb194243ea934d9e30900bf
agankap <correia.mig.ang@gmail.com>
2024-11-09 23:14:50 +0100

    [ALU] - alu.v with comment

--- alu/alu.v
+++ alu/alu.v
@@ -9,17 +9,17 @@ module alu (
 
     always @* begin
         case (op_code)
-           `ALU_ADD:   result = input_a +    input_b;
-           `ALU_SUB:    result = input_a -    input_b;
-           `ALU_SLL:    result = input_a <<   input_b[4:0];
-           `ALU_SRL:    result = input_a >>   input_b[4:0];
-           `ALU_SRA:    result = input_a >>>  input_b[4:0];
-           `ALU_SEQ:    result = {31'b0, input_a == input_b};
-           `ALU_SLT:    result = {31'b0, input_a < input_b};
-           `ALU_SLTU:   result = {31'b0, $unsigned(input_a) < $unsigned(input_b)};
-           `ALU_XOR:    result = input_a ^    input_b;
-           `ALU_OR:     result = input_a |    input_b;
-           `ALU_AND:    result = input_a &    input_b;
+           `ALU_ADD:   result = input_a +    input_b; //addition
+           `ALU_SUB:    result = input_a -    input_b; //subtraction
+           `ALU_SLL:    result = input_a <<   input_b[4:0]; //Shift less than 
+           `ALU_SRL:    result = input_a >>   input_b[4:0]; //Shift Right logic
+           `ALU_SRA:    result = input_a >>>  input_b[4:0]; //Shift Right Aritmetic
+           `ALU_SEQ:    result = {31'b0, input_a == input_b}; //Set qual
+           `ALU_SLT:    result = {31'b0, input_a < input_b}; //Set less Than (signed)
+           `ALU_SLTU:   result = {31'b0, $unsigned(input_a) < $unsigned(input_b)}; //set less than (unsigned)
+           `ALU_XOR:    result = input_a ^    input_b; //Logical XOR
+           `ALU_OR:     result = input_a |    input_b; //Logical OR
+           `ALU_AND:    result = input_a &    input_b; //Logical AND
             default: result= `ZERO;
         endcase
 
